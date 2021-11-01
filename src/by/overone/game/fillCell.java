c8c6400956321dcfa6028debecf06e8a92ef541c
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-01 21:57:17 +0300

    method fill cell by computer

--- src/by/overone/game/fillCell.java
+++ src/by/overone/game/fillCell.java
@@ -1,17 +1,35 @@
 package by.overone.game;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class fillCell {
 
-    public static boolean checkCell(String key){
+    public static boolean checkCell(String key) {
         return gameBox.box.get(key).equals(" ");
     }
 
-    public static void addInCell(String key, String ans){
-        if(checkCell(key)){
+    public static void addInCell(String key) {
+        String ans = "X";
+        if (checkCell(key)) {
             gameBox.box.replace(key, ans);
-        }
-        else{
+        } else {
             System.out.println("Cell isn't empty");
         }
     }
+
+    public static void addInCellComp() {
+        String ans = "O";
+        Random r = new Random();
+        List<String> keys = new ArrayList<>(gameBox.box.keySet());
+        String randomKey = keys.get(r.nextInt(keys.size()));
+
+        if (checkCell(randomKey)) {
+            gameBox.box.replace(randomKey, ans);
+        } else{
+            addInCellComp();
+        }
+    }
+
 }
