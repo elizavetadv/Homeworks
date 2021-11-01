092a6bc4d3e75ba91148c44145eccbd5294dcfb5
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-01 22:19:52 +0300

    change Main in according to previous change

--- src/by/overone/game/Main.java
+++ src/by/overone/game/Main.java
@@ -1,25 +1,9 @@
 package by.overone.game;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Tic-Tac-Toe game!");
-        gameBox.createBox();
-        gameBox.printBox();
 
-        System.out.print("\nYour turn. Enter 2 numbers of cell, separated by a space: ");
-        Scanner sc = new Scanner(System.in);
-        String ans =sc.nextLine();
+        fullGameCircle.oneGame();
 
-        fillCell.addInCell(ans);
-
-        gameBox.printBox();
-
-        System.out.println("\nComputer's turn");
-
-        fillCell.addInCellComp();
-
-        gameBox.printBox();
     }
 }
