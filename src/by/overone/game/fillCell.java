1c37e7bb7491accef86a224a4e7faf3cfd3f30a8
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-04 21:28:17 +0300

    create method stopOrMakeWin

--- src/by/overone/game/fillCell.java
+++ src/by/overone/game/fillCell.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 public class fillCell {
 
+    public static int valueStopUserWin;
+
     public static boolean checkCell(String key) {
         return gameBox.box.get(key).equals(" ");
     }
@@ -20,6 +22,7 @@ public class fillCell {
     }
 
     public static void addInCellComp() {
+
         String ans = "O";
         Random r = new Random();
         List<String> keys = new ArrayList<>(gameBox.box.keySet());
@@ -27,9 +30,135 @@ public class fillCell {
 
         if (checkCell(randomKey)) {
             gameBox.box.replace(randomKey, ans);
-        } else{
+        } else {
             addInCellComp();
         }
+
+    }
+
+    public static boolean stopOrMakeWin(String player) {
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("1 2").equals(player) && gameBox.box.get("1 3").equals(" ")) {
+            gameBox.box.replace("1 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("1 3").equals(player) && gameBox.box.get("1 2").equals(" ")) {
+            gameBox.box.replace("1 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 2").equals(player) && gameBox.box.get("1 3").equals(player) && gameBox.box.get("1 1").equals(" ")) {
+            gameBox.box.replace("1 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("2 3").equals(" ")) {
+            gameBox.box.replace("2 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 1").equals(player) && gameBox.box.get("2 3").equals(player) && gameBox.box.get("2 2").equals(" ")) {
+            gameBox.box.replace("2 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 2").equals(player) && gameBox.box.get("2 3").equals(player) && gameBox.box.get("2 1").equals(" ")) {
+            gameBox.box.replace("2 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("3 2").equals(player) && gameBox.box.get("3 3").equals(" ")) {
+            gameBox.box.replace("3 3", "O");
+            valueStopUserWin = 1;
+        }
+
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("3 2").equals(" ")) {
+            gameBox.box.replace("3 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("3 2").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("3 1").equals(" ")) {
+            gameBox.box.replace("3 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("2 1").equals(player) && gameBox.box.get("3 1").equals(" ")) {
+            gameBox.box.replace("3 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("3 1").equals(player) && gameBox.box.get("2 1").equals(" ")) {
+            gameBox.box.replace("2 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 1").equals(player) && gameBox.box.get("3 1").equals(player) && gameBox.box.get("1 1").equals(" ")) {
+            gameBox.box.replace("1 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 2").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 2").equals(" ")) {
+            gameBox.box.replace("3 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 2").equals(player) && gameBox.box.get("3 2").equals(player) && gameBox.box.get("2 2").equals(" ")) {
+            gameBox.box.replace("2 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 2").equals(player) && gameBox.box.get("1 2").equals(" ")) {
+            gameBox.box.replace("1 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 3").equals(player) && gameBox.box.get("2 3").equals(player) && gameBox.box.get("3 3").equals(" ")) {
+            gameBox.box.replace("3 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 3").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("2 3").equals(" ")) {
+            gameBox.box.replace("2 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 3").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("1 3").equals(" ")) {
+            gameBox.box.replace("1 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 3").equals(" ")) {
+            gameBox.box.replace("3 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("2 2").equals(" ")) {
+            gameBox.box.replace("2 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 3").equals(player) && gameBox.box.get("1 1").equals(" ")) {
+            gameBox.box.replace("1 1", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("1 3").equals(" ")) {
+            gameBox.box.replace("1 3", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("1 3").equals(player) && gameBox.box.get("2 2").equals(" ")) {
+            gameBox.box.replace("2 2", "O");
+            return false;
+        }
+
+        if (gameBox.box.get("2 2").equals(player) && gameBox.box.get("1 3").equals(player) && gameBox.box.get("3 1").equals(" ")) {
+            gameBox.box.replace("3 1", "O");
+            return false;
+        }
+
+        return true;
     }
 
 }
