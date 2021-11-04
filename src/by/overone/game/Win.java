2615c1096828da58ccff5e3c8ac724d1b457bc85
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-04 11:43:42 +0300

    fix checkWin

--- src/by/overone/game/Win.java
+++ src/by/overone/game/Win.java
@@ -10,47 +10,47 @@ public class Win {
         }
     }
 
-    public static boolean checkWin() {
+    public static boolean checkWin(String player) {
         boolean flag = false;
 
-        if (gameBox.box.get("1 1").equals(gameBox.box.get("1 2")) && gameBox.box.get("1 2").equals(gameBox.box.get("1 3")) && !gameBox.box.get("1 1").equals(" ")) {
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("1 2").equals(player) && gameBox.box.get("1 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("1 1"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("2 1").equals(gameBox.box.get("2 2")) && gameBox.box.get("2 2").equals(gameBox.box.get("2 3")) && !gameBox.box.get("2 1").equals(" ")) {
+        if (gameBox.box.get("2 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("2 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("2 1"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("3 1").equals(gameBox.box.get("3 2")) && gameBox.box.get("3 2").equals(gameBox.box.get("3 3")) && !gameBox.box.get("3 1").equals(" ")) {
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("3 2").equals(player) && gameBox.box.get("3 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("3 1"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("1 1").equals(gameBox.box.get("2 1")) && gameBox.box.get("2 1").equals(gameBox.box.get("3 1")) && !gameBox.box.get("1 1").equals(" ")) {
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("2 1").equals(player) && gameBox.box.get("3 1").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("1 1"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("1 2").equals(gameBox.box.get("2 2")) && gameBox.box.get("2 2").equals(gameBox.box.get("3 2")) && !gameBox.box.get("1 2").equals(" ")) {
+        if (gameBox.box.get("1 2").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 2").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("1 2"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("1 3").equals(gameBox.box.get("2 3")) && gameBox.box.get("2 3").equals(gameBox.box.get("3 3")) && !gameBox.box.get("1 3").equals(" ")) {
+        if (gameBox.box.get("1 3").equals(player) && gameBox.box.get("2 3").equals(player) && gameBox.box.get("3 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("1 3"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("1 1").equals(gameBox.box.get("2 2")) && gameBox.box.get("2 2").equals(gameBox.box.get("3 3")) && !gameBox.box.get("1 1").equals(" ")) {
+        if (gameBox.box.get("1 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("3 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("1 1"));
+            whoWin(player);
         }
 
-        if (gameBox.box.get("3 1").equals(gameBox.box.get("2 2")) && gameBox.box.get("2 2").equals(gameBox.box.get("1 3")) && !gameBox.box.get("3 1").equals(" ")) {
+        if (gameBox.box.get("3 1").equals(player) && gameBox.box.get("2 2").equals(player) && gameBox.box.get("1 3").equals(player)) {
             flag = true;
-            whoWin(gameBox.box.get("3 1"));
+            whoWin(player);
         }
 
         return flag;
