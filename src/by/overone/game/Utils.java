f0db7eeb0c015aae2db652a712606355715ee10c
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-05 12:32:25 +0300

    create method for entering and checking user answer

--- src/by/overone/game/Utils.java
+++ src/by/overone/game/Utils.java
@@ -1,10 +1,31 @@
 package by.overone.game;
 
+import java.util.Scanner;
+
 public class Utils {
+
     public static boolean checkEnteredData(String str) {
 
         return str.equals("1 1") || str.equals("1 2") || str.equals("1 3") || str.equals("2 1") || str.equals("2 2")
                 || str.equals("2 3") || str.equals("3 1") || str.equals("3 2") || str.equals("3 3");
 
     }
+
+    public static String enterCorrectValue(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nYour turn. Enter 2 numbers of cell, separated by a space: ");
+        String ans = sc.nextLine();
+
+        while(!Utils.checkEnteredData(ans)){
+
+            System.out.println("\nInvalid data...");
+            System.out.print("Your turn. Enter 2 numbers of cell, separated by a space: ");
+            ans = sc.nextLine();
+
+        }
+
+        return ans;
+    }
+
 }
