88cdb9dcf11b87aafaf9abd2b459ccea3d11a65e
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-07 17:41:31 +0300

    add functions

--- src/by/overone/hw23/Main.java
+++ src/by/overone/hw23/Main.java
@@ -2,6 +2,13 @@ package by.overone.hw23;
 
 public class Main {
     public static void main(String[] args) {
+        Num<Integer> num = new Num<>();
+        Num<Integer> num1 = new Num<>();
+
+        num.setValue(1);
+        num1.setValue(2);
+
+        System.out.println(num.sum(num1));
 
     }
 }
--- src/by/overone/hw23/Num.java
+++ src/by/overone/hw23/Num.java
@@ -1,4 +1,45 @@
 package by.overone.hw23;
 
-public class Num {
+public class Num<T extends Number> {
+    private T value;
+
+    public T sum(Num<?> value1) {
+
+        if (value instanceof Integer) {
+            return (T) Integer.valueOf(value.intValue() + value1.getValue().intValue());
+        } else if (value instanceof Double) {
+            return (T) Double.valueOf(value.doubleValue() + value1.getValue().doubleValue());
+        } else if (value instanceof Byte) {
+            return (T) Byte.valueOf((byte) (value.byteValue() + value1.getValue().byteValue()));
+        } else if (value instanceof Float) {
+            return (T) Float.valueOf(value.floatValue() + value1.getValue().floatValue());
+        } else if (value instanceof Long) {
+            return (T) Long.valueOf(value.longValue() + value1.getValue().longValue());
+        } else if (value instanceof Short) {
+            return (T) Short.valueOf((short) (value.shortValue() + value1.getValue().shortValue()));
+        } else {
+            throw new IllegalArgumentException("Type " + value.getClass() + " is not supported by this method");
+        }
+
+    }
+
+    public T difference(Num<?> value1) {
+        return getValue();
+    }
+
+    public T composition(Num<?> value1) {
+        return getValue();
+    }
+
+    public T division(Num<?> value1) {
+        return getValue();
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
