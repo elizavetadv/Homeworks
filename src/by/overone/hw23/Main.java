22751f5c747e2ba9195002398e6b233a1a95a5a0
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-09 20:55:31 +0300

    checking work of methods

--- src/by/overone/hw23/Main.java
+++ src/by/overone/hw23/Main.java
@@ -3,12 +3,31 @@ package by.overone.hw23;
 public class Main {
     public static void main(String[] args) {
         Num<Integer> num = new Num<>();
-        Num<Integer> num1 = new Num<>();
+        Num<Long> num1 = new Num<>();
 
-        num.setValue(1);
-        num1.setValue(2);
+        num.setValue(10);
+        num1.setValue(1000000000000000L);
 
-        System.out.println(num.sum(num1));
+        //throw an exception "Type class java.lang.Long does not fit in the type class java.lang.Integer"
+        //System.out.println("Sum: " + num.sum(num1));
+
+        Num<Double> num2 = new Num<>();
+
+        num2.setValue(1.9);
+
+        System.out.println("Difference: " + num.giveResult(num.difference(num2), num2));
+
+        Num<Integer> num3 = new Num<>();
+
+        num3.setValue(5);
+
+        System.out.println("Composition: " + num.giveResult(num.composition(num3), num3));
+
+        Num<Float> num4 = new Num<>();
+
+        num4.setValue(2.0f);
+
+        System.out.println("Division: " + num.giveResult(num.division(num4), num4));
 
     }
 }
