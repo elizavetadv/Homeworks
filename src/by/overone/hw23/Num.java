7bc23ca2368b9afc34ab2e497f1956bfed6ed6f6
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-09 20:54:59 +0300

    realization of methods

--- src/by/overone/hw23/Num.java
+++ src/by/overone/hw23/Num.java
@@ -3,36 +3,84 @@ package by.overone.hw23;
 public class Num<T extends Number> {
     private T value;
 
-    public T sum(Num<?> value1) {
-
-        if (value instanceof Integer) {
-            return (T) Integer.valueOf(value.intValue() + value1.getValue().intValue());
-        } else if (value instanceof Double) {
-            return (T) Double.valueOf(value.doubleValue() + value1.getValue().doubleValue());
-        } else if (value instanceof Byte) {
-            return (T) Byte.valueOf((byte) (value.byteValue() + value1.getValue().byteValue()));
-        } else if (value instanceof Float) {
-            return (T) Float.valueOf(value.floatValue() + value1.getValue().floatValue());
-        } else if (value instanceof Long) {
-            return (T) Long.valueOf(value.longValue() + value1.getValue().longValue());
-        } else if (value instanceof Short) {
-            return (T) Short.valueOf((short) (value.shortValue() + value1.getValue().shortValue()));
-        } else {
-            throw new IllegalArgumentException("Type " + value.getClass() + " is not supported by this method");
+    public T giveResult(Double result, Num<?> value1){
+        if (value instanceof Long) {
+            if(result >= Long.MIN_VALUE && result <= Long.MAX_VALUE) {
+                return (T) Long.valueOf(result.longValue());
+            } else{
+                throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+            }
         }
 
+        if (value instanceof Double) {
+            if(result >= Double.MIN_VALUE && result <= Double.MAX_VALUE) {
+                return (T) Double.valueOf(result);
+            } else{
+                throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+            }
+        }
+
+        if (value instanceof Integer) {
+
+            if (value1.getValue() instanceof Long) {
+                try {
+                    Math.toIntExact((long) value1.getValue());
+                } catch (ArithmeticException e) {
+                    throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+                }
+            } else {
+                return (T) Integer.valueOf(result.intValue());
+            }
+
+        }
+
+        if (value instanceof Float) {
+            if(result >= Float.MIN_VALUE && result <= Float.MAX_VALUE) {
+                return (T) Float.valueOf(result.floatValue());
+            } else{
+                throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+            }
+        }
+
+        if (value instanceof Short) {
+            if(result >= Short.MIN_VALUE && result <= Short.MAX_VALUE) {
+                return (T) Short.valueOf(result.shortValue());
+            } else{
+                throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+            }
+        }
+
+        if (value instanceof Byte) {
+            if(result >= Byte.MIN_VALUE && result <= Byte.MAX_VALUE) {
+                return (T) Byte.valueOf(result.byteValue());
+            } else{
+                throw new IllegalArgumentException("Type " + value1.getValue().getClass() + " does not fit in the type " + value.getClass());
+            }
+        }
+
+        return null;
     }
 
-    public T difference(Num<?> value1) {
-        return getValue();
+    public Double sum(Num<?> value1) {
+        return value.doubleValue() + value1.getValue().doubleValue();
     }
 
-    public T composition(Num<?> value1) {
-        return getValue();
+    public Double difference(Num<?> value1) {
+        return value.doubleValue() - value1.getValue().doubleValue();
+
     }
 
-    public T division(Num<?> value1) {
-        return getValue();
+    public Double composition(Num<?> value1) {
+        return value.doubleValue() * value1.getValue().doubleValue();
+    }
+
+    public Double division(Num<?> value1) {
+
+        if (value1.getValue().doubleValue() == 0) {
+            return null;
+        }
+
+        return value.doubleValue() / value1.getValue().doubleValue();
     }
 
     public T getValue() {
