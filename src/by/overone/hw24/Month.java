3db84624a6e3098c0ab48c6c38f05f1d0f2ca316
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-12 14:41:06 +0300

    add number of month and method returned month

--- src/by/overone/hw24/Month.java
+++ src/by/overone/hw24/Month.java
@@ -1,9 +1,46 @@
 package by.overone.hw24;
 
 public enum Month {
-    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+    JANUARY(1), FEBRUARY(2), MARCH(3), APRIL(4), MAY(5), JUNE(6), JULY(7), AUGUST(8), SEPTEMBER(9), OCTOBER(10), NOVEMBER(11), DECEMBER(12);
 
-    public static String getMonth(String str) {
+    private int number;
+
+    Month(int number) {
+        this.number = number;
+    }
+
+    public static Month getMonth(String str) {
+
+        switch (valueOf(str.toUpperCase())) {
+            case JANUARY:
+                return JANUARY;
+            case FEBRUARY:
+                return FEBRUARY;
+            case MARCH:
+                return MARCH;
+            case APRIL:
+                return APRIL;
+            case MAY:
+                return MAY;
+            case JUNE:
+                return JUNE;
+            case JULY:
+                return JULY;
+            case AUGUST:
+                return AUGUST;
+            case SEPTEMBER:
+                return SEPTEMBER;
+            case OCTOBER:
+                return OCTOBER;
+            case NOVEMBER:
+                return NOVEMBER;
+            case DECEMBER:
+                return DECEMBER;
+        }
+        return null;
+    }
+
+    public static String getStrMonth(String str) {
 
         switch (valueOf(str.toUpperCase())) {
             case JANUARY:
@@ -33,5 +70,13 @@ public enum Month {
             default:
                 return null;
         }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
