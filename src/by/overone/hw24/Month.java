2010ffc4973d2ff817740adf701122ac55b27f3c
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-12 13:22:09 +0300

    create month enum and method of getting month

--- src/by/overone/hw24/Month.java
+++ src/by/overone/hw24/Month.java
@@ -1,5 +1,37 @@
 package by.overone.hw24;
 
 public enum Month {
-    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+
+    public static String getMonth(String str) {
+
+        switch (valueOf(str.toUpperCase())) {
+            case JANUARY:
+                return JANUARY.toString();
+            case FEBRUARY:
+                return FEBRUARY.toString();
+            case MARCH:
+                return MARCH.toString();
+            case APRIL:
+                return APRIL.toString();
+            case MAY:
+                return MAY.toString();
+            case JUNE:
+                return JUNE.toString();
+            case JULY:
+                return JULY.toString();
+            case AUGUST:
+                return AUGUST.toString();
+            case SEPTEMBER:
+                return SEPTEMBER.toString();
+            case OCTOBER:
+                return OCTOBER.toString();
+            case NOVEMBER:
+                return NOVEMBER.toString();
+            case DECEMBER:
+                return DECEMBER.toString();
+            default:
+                return null;
+        }
+    }
 }
