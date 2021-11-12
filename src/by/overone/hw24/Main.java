bbec8a48cebd9b2400169d209a7543f5841b6c09
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-12 13:47:56 +0300

    create dialog with user

--- src/by/overone/hw24/Main.java
+++ src/by/overone/hw24/Main.java
@@ -1,7 +1,27 @@
 package by.overone.hw24;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+//        System.out.print("Enter day and month of your birth (ex: 20 august): ");
+        System.out.print("Enter day of your birth: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int day = sc.nextInt();
+
+        System.out.print("Enter month of your birth: ");
+
+        String ans = sc.next();
+
+        Birthday birthday = new Birthday();
+        birthday.setUserDay(day);
+
+        birthday.monthBh.setUserMonth(ans);
+
+        System.out.println(birthday.monthBh.getUserMonth());
 
     }
 }
