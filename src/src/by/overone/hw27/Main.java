8d9df840a0d597c2ba58d15138ba225e6263bf18
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-21 20:57:30 +0300

    find popular and not popular words and write them in file

--- src/src/by/overone/hw27/Main.java
+++ src/src/by/overone/hw27/Main.java
@@ -2,6 +2,10 @@ package src.by.overone.hw27;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -20,13 +24,32 @@ public class Main {
 
         in.close();
 
-        System.out.println(result);
+        String s = result.toString();
+
+        writeResult("Most popular word is ");
+        String word1 = Arrays.stream(s.split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+       writeResult(word1);
+
+        writeResult("\nMost unpopular word is ");
+        String word2 = Arrays.stream(s.split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .min(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+        writeResult(word2);
+
     }
 
     private static void downloadFromInternet() throws IOException {
         URL url = new URL("https://shakespeare.folger.edu/downloads/txt/romeo-and-juliet_TXT_FolgerShakespeare.txt");
 
-        BufferedInputStream bufferedInputStream= new BufferedInputStream(url.openStream());
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(url.openStream());
         FileOutputStream fileOutputStream = new FileOutputStream("roman.txt");
 
         byte[] buffer = new byte[1024];
@@ -38,5 +61,12 @@ public class Main {
 
         fileOutputStream.close();
         bufferedInputStream.close();
+    }
+
+    public static void writeResult(String text) throws IOException {
+        FileWriter fileWriter = new FileWriter("result.txt", true);
+        fileWriter.write("");
+        fileWriter.append(text);
+        fileWriter.close();
     }
 }
