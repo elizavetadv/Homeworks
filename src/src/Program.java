7f3fc3cd746de707c13a0773cda6b3e8f1d155bd
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-26 17:32:29 +0300

    create general program for store

---
+++ src/src/Program.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Program {
+    public static void main(String[] args) {
+        Store store = new Store();
+        Producer producer = new Producer(store);
+        Consumer consumer = new Consumer(store);
+
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
+}
