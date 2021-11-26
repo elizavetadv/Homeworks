58f640ed8d76bef236065a61965fcf08b094a93f
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-26 17:18:32 +0300

    create threads

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,11 @@
 package src;
 
 public class Main {
+    public static void main(String[] args) {
+        Thread hen = new Thread(new Hen(), "It is a hen");
+        Thread egg = new Thread(new Egg(), "It is an egg");
+
+        hen.start();
+        egg.start();
+    }
 }
