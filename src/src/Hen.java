9946d15dedf061d6f5e26b819b688a50366852b3
Elizaveta Davidovich <lizavetka.davidovich@gmail.com>
2021-11-26 17:17:12 +0300

    realize Hen class

--- src/src/Hen.java
+++ src/src/Hen.java
@@ -1,4 +1,14 @@
 package src;
 
-public class Hen {
+public class Hen  implements Runnable{
+
+    @Override
+    public void run() {
+        int i = 0;
+        while(i < 3) {
+            System.out.println("Hen " + i );
+            i++;
+        }
+    }
+
 }
